028ce3d38a7bea2be5a241a03ce3544d2e128d28
Muhammad Ali Cheema <66263526+MuhammadAli077@users.noreply.github.com>
2020-06-14 02:31:16 +0700

    update basic react state

---
+++ src/components/Disclaimer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Disclaimer = () => {
+  return (
+    <h10>
+         ALL RIGHTS RESERVED
+     
+    </h10>
+  )
+}
---
+++ src/components/Personal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Personal = () => {
+  return (
+    <h11>
+       
+           𝐁𝐲 𝐌𝐮𝐡𝐚𝐦𝐦𝐚𝐝 𝐀𝐥𝐢 𝐂𝐡𝐞𝐞𝐦𝐚
+                            
+    </h11>
+  )
+}
